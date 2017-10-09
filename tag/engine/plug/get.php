3ed5335220644ef284a844e7c1de7aa97a0d396e
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-10-09 17:51:59 +0700

    Remove `Get::tag` Method

--- tag/engine/plug/get.php
+++ tag/engine/plug/get.php
@@ -2,13 +2,8 @@
 
 // Based on `lot\extend\page\engine\plug\get.php`
 
-function fn_get_tag($path, $key = null, $fail = false, $for = null) {
-    return call_user_func('fn_get_page', $path, $key, $fail, $for);
-}
-
 function fn_get_tags($folder = TAG, $state = 'page', $sort = [1, 'id'], $key = null) {
     return call_user_func('fn_get_pages', $folder, $state, $sort, $key);
 }
 
-Get::plug('tag', 'fn_get_tag');
 Get::plug('tags', 'fn_get_tags');
