fe423b1df4075403374d2869e95c824e4ccc6ff1
Taufik Nurrohman <t.nurrohman77@gmail.com>
2018-04-05 20:15:22 +0700

    @mecha-cms/genome/engine/kernel/genome.php

--- tag/engine/plug/get.php
+++ tag/engine/plug/get.php
@@ -1,9 +1,4 @@
 <?php
 
 // Based on `lot\extend\page\engine\plug\get.php`
-
-function fn_get_tags($folder = TAG, $state = 'page', $sort = [1, 'id'], $key = null) {
-    return fn_get_pages($folder, $state, $sort, $key);
-}
-
-Get::_('tags', 'fn_get_tags');
+Get::_('tags', ['fn_get_pages', [TAG, 'page', [1, 'id'], null]]);
