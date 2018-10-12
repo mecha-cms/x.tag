c73727ae6ab99a3a243fc62c8f86ecdfdebe2dd7
Taufik Nurrohman <t.nurrohman77@gmail.com>
2018-10-12 11:44:15 +0700

    PHP 7.1.0 and Above

--- tag/engine/kernel/tag.php
+++ tag/engine/kernel/tag.php
@@ -2,21 +2,17 @@
 
 class Tag extends Page {
 
-    public function __construct($input = [], $lot = [], $NS = []) {
+    public function __construct($path = null, array $lot = [], $NS = []) {
         global $site, $url;
-        $p = $url->path;
-        if (is_array($input)) {
-            $s = Path::N(isset($input['path']) ? $input['path'] : "");
-        } else {
-            $s = Path::N($input);
-        }
+        $p = $GLOBALS['URL']['path'];
+        $n = $path ? Path::N($path) : null;
         if ($site->is('tags')) { // → `blog/tag/tag-slug`
             $p = Path::D($p, 2);
         } else if ($site->is('page')) { // → `blog/page-slug`
             $p = Path::D($p);
         }
-        parent::__construct($input, array_replace([
-            'url' => $s ? $url . ($p ? '/' . $p : "") . '/' . Extend::state('tag', 'path') . '/' . $s : null
+        parent::__construct($path, array_replace([
+            'url' => $n ? $GLOBALS['URL']['$'] . ($p ? '/' . $p : "") . '/' . Extend::state('tag', 'path') . '/' . $n : null
         ], $lot), $NS);
     }
 
