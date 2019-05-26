c3067019398a2522ca602fd4cce4e9e5afb69ab7
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-05-26 08:54:14 +0700

    Update

--- tag/engine/r/config.php
+++ tag/engine/r/config.php
@@ -4,6 +4,6 @@
 $state = extend('tag');
 if (!empty($state['tag'])) {
     // Prioritize default state
-    Config::alt($state);
+    Config::over($state);
     User::$data = array_replace_recursive(Page::$data, (array) Config::get('tag', true));
 }
