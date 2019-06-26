4275599f247bd626cdf48b94ad1643cdd472df3a
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-06-26 21:51:39 +0700

    https://github.com/mecha-cms/genome/issues/55

--- tag/engine/kernel/tags.php
+++ tag/engine/kernel/tags.php
@@ -1,3 +1,41 @@
-<?php // TODO
+<?php
 
-class Tags extends Anemon {}
+class Comments extends Anemon {
+
+    public function getIterator() {
+        $tags = [];
+        foreach ($this->value as $v) {
+            $tags[] = new Tag($v);
+        }
+        return new \ArrayIterator($tags);
+    }
+
+    public function sort($sort = 1, $preserve_key = false) {
+        if (is_array($sort)) {
+            $value = [];
+            foreach ($this->value as $v) {
+                $value[$v] = (new Tag($v))[$sort[1]];
+            }
+            $sort[0] === -1 ? arsort($value) : asort($value);
+            $this->value = array_keys($value);
+        } else {
+            $value = $this->value;
+            if ($preserve_key) {
+                $sort === -1 ? arsort($value) : asort($value);
+            } else {
+                $sort === -1 ? rsort($value) : sort($value);
+            }
+            $this->value = $value;
+        }
+        return $this;
+    }
+
+    public function take(string $key, $or = null) {
+        $value = [];
+        foreach ($this->value as $v) {
+            $value[] = (new Tags($v))[$key] ?? $or;
+        }
+        return $value;
+    }
+
+}
