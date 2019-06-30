a12242bac47bffb2766a3a15c0d94c3b34d58338
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-06-30 12:04:31 +0700

    Update tags.php

--- tag/engine/kernel/tags.php
+++ tag/engine/kernel/tags.php
@@ -1,41 +1,9 @@
 <?php
 
-class Tags extends Anemon {
+class tags extends Pages {
 
-    public function getIterator() {
-        $tags = [];
-        foreach ($this->value as $v) {
-            $tags[] = new Tag($v);
-        }
-        return new \ArrayIterator($tags);
-    }
-
-    public function pluck(string $key, $or = null) {
-        $value = [];
-        foreach ($this->value as $v) {
-            $value[] = (new Tags($v))[$key] ?? $or;
-        }
-        return $value;
-    }
-
-    public function sort($sort = 1, $preserve_key = false) {
-        if (is_array($sort)) {
-            $value = [];
-            foreach ($this->value as $v) {
-                $value[$v] = (new Tag($v))[$sort[1]];
-            }
-            $sort[0] === -1 ? arsort($value) : asort($value);
-            $this->value = array_keys($value);
-        } else {
-            $value = $this->value;
-            if ($preserve_key) {
-                $sort === -1 ? arsort($value) : asort($value);
-            } else {
-                $sort === -1 ? rsort($value) : sort($value);
-            }
-            $this->value = $value;
-        }
-        return $this;
+    public function page(string $path) {
+        return new Tag($path);
     }
 
 }
