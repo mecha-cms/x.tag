423cf7cb2d11c15ce569d6668de8012981c24b39
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-11-21 23:50:32 +0700

    Update tag.php

--- tag/engine/kernel/tag.php
+++ tag/engine/kernel/tag.php
@@ -2,6 +2,12 @@
 
 class Tag extends Page {
 
+    public function __construct(string $path = null, array $lot = []) {
+        $c = c2f(self::class);
+        parent::__construct($path, array_replace_recursive((array) State::get('x.' . $c . '.page', true), $lot));
+        $this->h[] = $c;
+    }
+
     public function URL(...$lot) {
         $url = $GLOBALS['url'];
         $p = trim($url->path, '/');
