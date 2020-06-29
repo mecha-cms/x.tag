3c692ab0314067961277b8fc52b380ec60100789
Taufik Nurrohman <t.nurrohman77@gmail.com>
2020-06-29 11:03:33 +0700

    Update

--- tag/engine/kernel/tag.php
+++ tag/engine/kernel/tag.php
@@ -9,9 +9,10 @@ class Tag extends Page {
     }
 
     public function URL(...$lot) {
+        extract($GLOBALS, EXTR_SKIP);
         $n = $this->exist ? Path::N($this->path) : null;
-        $path = $this->page->url ?? "";
-        return $n ? ($path ? dirname($path) : "") . ($state->x->tag->path ?? '/tag') . '/' . $n : null;
+        $path = $this->page->url ?? $url . $state->path . '/' . P;
+        return $n ? dirname($path) . ($state->x->tag->path ?? '/tag') . '/' . $n : null;
     }
 
 }
