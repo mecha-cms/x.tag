ad3b4396b871394fe6d8cae2a2ef2ac8532a4ebb
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-02-27 18:46:07 +0700

    Accept Array Input

--- tag/engine/kernel/tags.php
+++ tag/engine/kernel/tags.php
@@ -5,14 +5,17 @@ class Tags extends Pages {
     public $page = null;
     public $parent = null;
 
-    public function page(string $path) {
-        $tag = new Tag($path);
+    public function page(string $path = null, array $lot = []) {
+        $tag = new Tag($path, $lot);
         $tag->page = $this->page;
         $tag->parent = $this->parent;
         return $tag;
     }
 
     public static function from(...$lot) {
+        if (is_array($v = reset($lot))) {
+            return parent::from($v);
+        }
         $lot[0] = $lot[0] ?? LOT . D . 'tag';
         return parent::from(...$lot);
     }
