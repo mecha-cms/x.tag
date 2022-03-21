b88de301464f77a48df855f8e55481e4c2f10d1b
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-03-21 13:23:38 +0700

    Update

--- tag/engine/kernel/tag.php
+++ tag/engine/kernel/tag.php
@@ -5,12 +5,6 @@ class Tag extends Page {
     public $page = null;
     public $parent = null;
 
-    public function __construct(string $path = null, array $lot = []) {
-        $c = c2f(self::class);
-        parent::__construct($path, array_replace_recursive((array) State::get('x.' . $c . '.page', true), $lot));
-        $this->hook[] = $c;
-    }
-
     public function link(...$lot) {
         if ($v = parent::get('link')) {
             return $v;
