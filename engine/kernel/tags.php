ceae35ccb9d17f407cec8cdab406712fe88491f8
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-07-19 20:17:34 +0700

    Update

--- engine/kernel/tags.php
+++ engine/kernel/tags.php
@@ -8,7 +8,7 @@ class Tags extends Pages {
 
     public static function from(...$lot) {
         if (is_array($v = reset($lot))) {
-            return parent::from($v);
+            return new static($v);
         }
         $lot[0] = $lot[0] ?? LOT . D . 'tag';
         return parent::from(...$lot);
