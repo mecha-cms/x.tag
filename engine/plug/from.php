a73eb4765e619c1322030b4283dbc9804f6fb40a
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-11-17 21:13:56 +0700

    Update

--- engine/plug/from.php
+++ engine/plug/from.php
@@ -1,21 +1,21 @@
 <?php
 
 From::_('tag', static function ($name) {
-    if (is_string($name)) {
-        $folder = LOT . D . 'tag' . D . $name;
-        // Get from external `id` data
-        if (is_file($file = $folder . D . 'id.data') && filesize($file) > 0) {
-            $id = trim((string) fgets($h = fopen($file, 'r')));
-            fclose($h);
-            return is_numeric($id) ? (int) $id : $id;
-        } else if ($file = exist([
-            $folder . '.archive',
-            $folder . '.page'
-        ], 1)) {
-            // Get from internal `id` data
-            return From::page(file_get_contents($file), true)['id'] ?? null;
-        }
+    if (!is_string($name)) {
+        return null;
     }
-    // Else…
+    $folder = LOT . D . 'tag' . D . $name;
+    // Get from external `id` data
+    if (is_file($file = $folder . D . 'id.data') && filesize($file) > 0) {
+        $id = trim((string) fgets(fopen($file, 'r')));
+        return is_numeric($id) ? (int) $id : $id;
+    }
+    if ($file = exist([
+        $folder . '.archive',
+        $folder . '.page'
+    ], 1)) {
+        // Get from internal `id` data
+        return From::page(file_get_contents($file))->id ?? null;
+    }
     return null;
 });
--- engine/plug/to.php
+++ engine/plug/to.php
@@ -1,19 +1,19 @@
 <?php
 
 To::_('tag', static function ($id) {
-    if (is_int($id)) {
-        // Search for external `id` data
-        foreach (glob(LOT . D . 'tag' . D . '*' . D . 'id.data', GLOB_NOSORT) as $v) {
-            if (is_file($v) && $id === (int) trim((string) fgets($h = fopen($v, 'r')))) {
-                fclose($h);
-                return basename(dirname($v));
-            }
+    if (!is_int($id)) {
+        return null;
+    }
+    // Search for external `id` data
+    foreach (glob(LOT . D . 'tag' . D . '*' . D . 'id.data', GLOB_NOSORT) as $v) {
+        if (is_file($v) && $id === (int) trim((string) fgets(fopen($v, 'r')))) {
+            return basename(dirname($v));
         }
-        // Search for embedded `id` data
-        foreach (glob(LOT . D . 'tag' . D . '*.{archive,page}', GLOB_BRACE | GLOB_NOSORT) as $v) {
-            if (is_file($v) && $id === (From::page(file_get_contents($v), true)['id'] ?? null)) {
-                return pathinfo($v, PATHINFO_FILENAME);
-            }
+    }
+    // Search for internal `id` data
+    foreach (glob(LOT . D . 'tag' . D . '*.{archive,page}', GLOB_BRACE | GLOB_NOSORT) as $v) {
+        if (is_file($v) && $id === (From::page(file_get_contents($v))->id ?? null)) {
+            return pathinfo($v, PATHINFO_FILENAME);
         }
     }
     return null;
