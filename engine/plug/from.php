d0960f82616d5be65ee665db30a10c8c051ea4c1
Taufik Nurrohman <hi@taufik-nurrohman.com>
2025-01-28 23:35:53 +0700

    In-Memory Cache

--- engine/plug/from.php
+++ engine/plug/from.php
@@ -4,18 +4,24 @@ From::_('tag', static function ($name) {
     if (!is_string($name)) {
         return null;
     }
+    static $cache = [];
+    if (isset($cache[$name])) {
+        return $cache[$name];
+    }
     $folder = LOT . D . 'tag' . D . $name;
     // Get from external `id` data
     if (is_file($file = $folder . D . 'id.data') && filesize($file) > 0) {
         $id = trim((string) fgets(fopen($file, 'r')));
-        return is_numeric($id) ? (int) $id : $id;
+        return ($cache[$name] = is_numeric($id) ? (int) $id : $id);
     }
     if ($file = exist([
         $folder . '.archive',
         $folder . '.page'
     ], 1)) {
         // Get from internal `id` data
-        return From::page(file_get_contents($file))->id ?? null;
+        if (null !== ($id = From::page(file_get_contents($file))->id ?? null)) {
+            return ($cache[$name] = $id);
+        }
     }
     return null;
 });
--- engine/plug/to.php
+++ engine/plug/to.php
@@ -4,16 +4,20 @@ To::_('tag', static function ($id) {
     if (!is_int($id)) {
         return null;
     }
+    static $cache = [];
+    if (isset($cache[$id])) {
+        return $cache[$id];
+    }
     // Search for external `id` data
     foreach (glob(LOT . D . 'tag' . D . '*' . D . 'id.data', GLOB_NOSORT) as $v) {
         if (is_file($v) && $id === (int) trim((string) fgets(fopen($v, 'r')))) {
-            return basename(dirname($v));
+            return ($cache[$id] = basename(dirname($v)));
         }
     }
     // Search for internal `id` data
     foreach (glob(LOT . D . 'tag' . D . '*.{archive,page}', GLOB_BRACE | GLOB_NOSORT) as $v) {
         if (is_file($v) && $id === (From::page(file_get_contents($v))->id ?? null)) {
-            return pathinfo($v, PATHINFO_FILENAME);
+            return ($cache[$id] = pathinfo($v, PATHINFO_FILENAME));
         }
     }
     return null;
