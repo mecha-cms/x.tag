7607c00999d4e58ecb4230df500ed6748c2aefed
Taufik Nurrohman <hi@taufik-nurrohman.com>
2025-02-28 15:24:00 +0700

    New Route(s)
     - `/…/tag/1`

--- index.php
+++ index.php
@@ -92,10 +92,13 @@ namespace x\tag {
         $part = ($part ?? 0) - 1;
         $page = $tag->parent ?? $tag;
         $chunk = $tag->chunk ?? $page->chunk ?? 5;
+        $deep = "" !== $path ? 0 : true;
         $sort = \array_replace([1, 'path'], (array) ($tag->sort ?? []), (array) ($page->sort ?? []));
+        // For `/tag/:name`, `/tag/:name/:part`, and `/…/tag/:name/:part`
         if ($name = \State::get('[x].query.tag') ?? "") {
             if (null !== ($id = \From::tag($name))) {
-                if ($part < 0) {
+                // For `/tag/:name`
+                if ($part < 0 && "" === $path) {
                     \lot('page', $tag);
                     \lot('t')[] = \i('Tag');
                     \lot('t')[] = $tag->title;
@@ -105,14 +108,13 @@ namespace x\tag {
                             'prev' => false
                         ]
                     ]);
-                    // For `/tag/:name`
                     return ['page/tag/' . $name, [], 200];
                 }
-                $pages = \Pages::from(\LOT . \D . 'page' . ("" !== $path ? \D . $path : ""), 'page', "" !== $path ? 0 : true)->sort($sort);
+                $pages = \Pages::from(\LOT . \D . 'page' . ("" !== $path ? \D . $path : ""), 'page', $deep)->sort($sort);
                 \State::set([
                     'chunk' => $chunk,
                     'count' => $count = $pages->count, // Total number of page(s) before chunk
-                    'deep' => true,
+                    'deep' => $deep,
                     'part' => $part + 1,
                     'sort' => $sort
                 ]);
@@ -128,7 +130,7 @@ namespace x\tag {
                 \lot('page', $page);
                 \lot('pager', $pager = $pager->chunk($chunk, $part));
                 \lot('pages', $pages = $pages->chunk($chunk, $part));
-                if (0 === ($count = $pages->count)) { // Total number of page(s) after chunk
+                if (0 === $pages->count) { // Total number of page(s) after chunk
                     \State::set([
                         'has' => [
                             'next' => false,
@@ -140,12 +142,11 @@ namespace x\tag {
                         ]
                     ]);
                     \lot('t')[] = \i('Error');
-                    // For `/…/tag/:name/:part`
                     return ['pages/tag/' . $name, [], 404];
                 }
                 \State::set('has', [
                     'next' => !!$pager->next,
-                    'pages' => $count > 0,
+                    'pages' => true,
                     'parent' => !!$tag->parent,
                     'prev' => !!$pager->prev
                 ]);
@@ -156,6 +157,84 @@ namespace x\tag {
         $chunk = $state->x->tag->chunk ?? 5;
         $deep = $state->x->tag->deep ?? 0;
         $sort = \array_replace([1, 'title'], (array) ($state->x->tag->sort ?? []));
+        // For `/…/tag/:part`
+        if ($path) {
+            $folder = \LOT . \D . 'page' . \D . $path;
+            $page = new \Page(\exist([
+                $folder . '.archive',
+                $folder . '.page'
+            ], 1) ?: null);
+            $tags = [];
+            foreach (\Pages::from($folder, 'page', $deep) as $v) {
+                if (!$kind = $v->kind) {
+                    continue;
+                }
+                foreach ((array) $kind as $k) {
+                    if (!$k = \To::tag($k)) {
+                        continue;
+                    }
+                    $k = \LOT . \D . 'tag' . \D . $k;
+                    if (!$k = \exist([
+                        $k . '.archive',
+                        $k . '.page'
+                    ], 1)) {
+                        continue;
+                    }
+                    $tags[$k] = [
+                        'parent' => $page->path,
+                        'path' => $k
+                    ];
+                }
+            }
+            $pages = (new \Tags(\array_values($tags)))->sort($sort);
+            if (0 === ($count = $pages->count)) {
+                return $content;
+            }
+            \State::set('count', $count);
+            $pager = \Pager::from($pages);
+            $pager->hash = $hash;
+            $pager->path = $path . '/' . $route;
+            $pager->query = $query;
+            \lot('page', $page);
+            \lot('pager', $pager = $pager->chunk($chunk, $part));
+            \lot('pages', $pages = $pages->chunk($chunk, $part));
+            \lot('t')[] = $page->title;
+            \lot('t')[] = \i('Tags');
+            if (0 === $pages->count) { // Total number of page(s) after chunk
+                \State::set([
+                    'has' => [
+                        'next' => false,
+                        'page' => false,
+                        'pages' => false,
+                        'parent' => !!$page->parent,
+                        'prev' => false
+                    ],
+                    'is' => [
+                        'error' => 404,
+                        'page' => false,
+                        'pages' => true
+                    ]
+                ]);
+                \lot('t')[] = \i('Error');
+                return ['pages/tags/' . $path, [], 404];
+            }
+            \State::set([
+                'has' => [
+                    'next' => !!$pager->next,
+                    'page' => true,
+                    'pages' => true,
+                    'parent' => !!$page->parent,
+                    'prev' => !!$pager->prev
+                ],
+                'is' => [
+                    'error' => false,
+                    'page' => false,
+                    'pages' => true
+                ]
+            ]);
+            return ['pages/tags', [], 200];
+        }
+        // For `/tag/:part`
         $pages = \Pages::from(\LOT . \D . 'tag', 'page')->sort($sort);
         if (0 === ($count = $pages->count)) { // Total number of page(s) before chunk
             return $content;
@@ -173,7 +252,7 @@ namespace x\tag {
         \lot('pager', $pager = $pager->chunk($chunk, $part));
         \lot('pages', $pages = $pages->chunk($chunk, $part));
         \lot('t')[] = $page->title;
-        if (0 === ($count = $pages->count)) { // Total number of page(s) after chunk
+        if (0 === $pages->count) { // Total number of page(s) after chunk
             \State::set([
                 'has' => [
                     'next' => false,
@@ -189,7 +268,6 @@ namespace x\tag {
                 ]
             ]);
             \lot('t')[] = \i('Error');
-            // For `/tag/:part`
             return ['pages/tags', [], 404];
         }
         \State::set([
@@ -206,13 +284,13 @@ namespace x\tag {
                 'pages' => true
             ]
         ]);
-        // For `/tag/:part`
         return ['pages/tags', [], 200];
     }
     if ($part = \x\page\n($path = \trim($url->path ?? "", '/'))) {
         $path = \substr($path, 0, -\strlen('/' . $part));
     }
     $route = \trim($state->x->tag->route ?? 'tag', '/');
+    // For `/tag/…`
     if (0 === \strpos($path . '/', $route . '/')) {
         \Hook::set('route.page', __NAMESPACE__ . "\\route__page", 90);
         \Hook::set('route.tag', __NAMESPACE__ . "\\route__tag", 100);
@@ -232,6 +310,7 @@ namespace x\tag {
             ],
             'part' => $part
         ]);
+        // For `/tag/:name/…`
         if ("" !== ($query = \substr($path, \strlen($route) + 1))) {
             \State::set('[x].query.tag', $query);
             $folder = \LOT . \D . 'tag' . \D . \strtr($query, '/', \D);
@@ -249,6 +328,7 @@ namespace x\tag {
     } else {
         $a = \explode('/', $path);
         $name = \array_pop($a);
+        // For `/…/tag/:part`
         if ($a && $part && $name === $route) {
             \Hook::set('route.page', __NAMESPACE__ . "\\route__page", 90);
             \Hook::set('route.tag', __NAMESPACE__ . "\\route__tag", 100);
@@ -271,6 +351,7 @@ namespace x\tag {
         } else {
             $r = \array_pop($a);
             $folder = \LOT . \D . 'tag' . \D . $name;
+            // For `/…/tag/:name/:part`
             if ($a && $part && $r === $route) {
                 \Hook::set('route.page', __NAMESPACE__ . "\\route__page", 90);
                 \Hook::set('route.tag', __NAMESPACE__ . "\\route__tag", 100);
